b3dbe3ba241ba13c2e3644cc9cfc9e39ac53bcac
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-03-15 17:07:04 -0700

    se agrego header al services menu para que no muestre el loading

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ComponenteInterface } from '@interfaces/componente-interface';
 
@@ -10,6 +10,10 @@ export class MenuService {
   constructor(private httpClient: HttpClient) { }
 
   getMenu(){
-    return this.httpClient.get<ComponenteInterface[]>('/assets/data/menu.json');
+    // Creamos el header para que el interceptor lo ignore
+    const headers = new HttpHeaders({
+      'skipLoading': 'true'
+    });
+    return this.httpClient.get<ComponenteInterface[]>('/assets/data/menu.json', { headers });
   }
 }
