54758773cc37bc761eeedbe21bef938331d4faf4
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-03-27 20:52:53 -0700

    se cambia la api prod en el environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: true,
-  // API: 'https://v2.sigmod.mx/api',
-  API: 'https://0f87-2806-269-486-206e-5d02-700d-8346-6f09.ngrok-free.app/api', 
-  //API: 'https://api.disi.gob.mx/api',
-  prefijoLocalStorage: 'disi_dev',
-  API_KEY:'6LdIDvYnAAAAAHVRcRyi8swoYq2oBeMlbGYREl2G',
-  API_EMAIL:'disi.mx@gmail.com'
+  API: 'https://aragobel-api-424137532274.us-central1.run.app/api', 
+  prefijoLocalStorage: 'aragobel_prod',
+  API_KEY:'',
+  API_EMAIL:''
 };
