db4f087d5baa3d7bf2b80c4e0cb9eec1684d6192
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-04-12 17:30:17 -0700

    se trabajo en el listado de Mis entregas

--- src/app/pages/entregas/list-entregas/list-entregas.component.ts
+++ src/app/pages/entregas/list-entregas/list-entregas.component.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { EntregaDbRepository } from '../../../repository/entrega-db.service';
 import { Entrega } from '@interfaces/entrega-interface'; 
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { PreferencesService } from '@services/preference.service';
 
 @Component({
   selector: 'app-list-entregas',
@@ -11,48 +14,103 @@ import { Entrega } from '@interfaces/entrega-interface';
   styleUrls: ['./list-entregas.component.scss'],
 })
 export class ListEntregasComponent  implements OnInit {
-  entregas = this.entregaDbRepository.entregas;
-  newCategoryName = '';
+  segmentoActual: string = 'disponibles';
+  todasLasEntregas: any[] = []; // Aquí llenas con tu API
+ 
+  idUsuarioActual: string = '';
+  puedeCrear: boolean = false; // Variable booleana para la vista
+  esCajero: boolean = false;
+  esAdmin: boolean = false;
 
-  constructor(private entregaDbRepository: EntregaDbRepository) { 
-    effect(() => {
-      console.log('CATEGORIES CHANGED ', this.entregas);
-    })
-  }
+  constructor(
+    private router: Router,
+    private alertCtrl: AlertController,
+    private _preferencesService: PreferencesService,
+  ) {}
 
   async ngOnInit() {
-    // Importante: Cargamos los datos al iniciar la página
-    await this.entregaDbRepository.loadAll();
+    this.puedeCrear = await this._preferencesService.tienePermiso('ENTREGAS', 'CREAR');
+    this.esAdmin = await this._preferencesService.esAdmin(); // O tu lógica de rol
+    this.idUsuarioActual = await this._preferencesService.getIdUser();
+    this.esCajero = this.puedeCrear;
+    // 3. Definimos qué pestaña mostrar por defecto al entrar
+    if (this.puedeCrear && !this.esAdmin) { //!this.esAdmin
+      this.esCajero = true;
+      this.segmentoActual = 'mis-registros';
+    } else {
+      this.esCajero = false;
+      this.segmentoActual = 'disponibles';
+    }
+    this.cargarDatos();
   }
 
-  async createCategory() {
-    if (!this.newCategoryName.trim()) return;
-
-    const ENTREGA_MOCK: Entrega = {
-      id_entrega: crypto.randomUUID(),
-      folio: this.newCategoryName.trim(),
-      id_repartidor: 'a1b2c3d4-e5f6-4g7h-8i9j-k0l1m2n3o4p5',
-      id_vehiculo: 'v9876543-21ba-4321-cba9-876543210fed',
-      colonia: 'Centro Histórico',
-      fec_registropedido: '2026-03-21T08:30:00Z',
-      fec_salidapedido: '2026-03-21T09:15:00Z',
-      fec_entregapedido: '2026-03-21T10:05:00Z',
-      id_tienda: 't1122334-4455-6677-8899-aabbccddeeff',
-      active: 1
-    };
-    
-    await this.entregaDbRepository.create(ENTREGA_MOCK);
-    this.newCategoryName = ''; // Limpiamos el input
+  cargarDatos() {
+    // Aquí llamarías a tu servicio: this.entregaService.getEntregas().subscribe(...)
+    // Ejemplo de datos basado en tu tabla SQL:
+    this.todasLasEntregas = [
+      { id: 1, folio: 'A-101', persona_recibe: 'Adrian', colonia: 'Centro', estatus: 1, id_repartidor: null , id_usuario_creador: 1},
+      { id: 2, folio: 'A-102', persona_recibe: 'Adrian2', colonia: 'Centro', estatus: 1, id_repartidor: null , id_usuario_creador: 1},
+      { id: 3, folio: 'A-103', persona_recibe: 'Adrian3', colonia: 'Centro', estatus: 1, id_repartidor: null , id_usuario_creador: 1},
+      { id: 4, folio: 'A-104', persona_recibe: 'Adrian4', colonia: 'Centro', estatus: 1, id_repartidor: null , id_usuario_creador: 1},
+      { id: 5, folio: 'A-105', persona_recibe: 'Jesus', colonia: 'Industrial', estatus: 2, id_repartidor: 1 , id_usuario_creador: 1},
+      { id: 6, folio: 'A-106', persona_recibe: 'Jesus1', colonia: 'Industrial', estatus: 2, id_repartidor: 1 , id_usuario_creador: 1},
+      { id: 7, folio: 'A-107', persona_recibe: 'Jesus2', colonia: 'Industrial', estatus: 3, id_repartidor: 1 , id_usuario_creador: 'blIL9Ts6MeEbubvhnVpP'},
+      { id: 8, folio: 'A-108', persona_recibe: 'Jesus3', colonia: 'Industrial', estatus: 1, id_repartidor: null , id_usuario_creador: 'blIL9Ts6MeEbubvhnVpP'},
+      { id: 9, folio: 'A-109', persona_recibe: 'Jesus4', colonia: 'Industrial', estatus: 2, id_repartidor: 1 , id_usuario_creador: 'blIL9Ts6MeEbubvhnVpP'},
+      { id: 9, folio: 'A-110', persona_recibe: 'Jesus5', colonia: 'Industrial2', estatus: 2, id_repartidor: 2, id_usuario_creador: 'blIL9Ts6MeEbubvhnVpP'}
+    ];
   }
 
-  async updateCategory(entrega: Entrega) {
-    // Invertimos el estado actual
-    const newStatus = entrega.active ? 0 : 1;
-    await this.entregaDbRepository.updateById(entrega.id_entrega, newStatus);
+  // El "motor" de las pestañas
+  get entregasFiltradas() {
+    if (this.esCajero) {
+      return this.todasLasEntregas.filter(e => {
+        // Filtro 1: Que el creador sea él mismo
+        const esMio = e.id_usuario_creador === this.idUsuarioActual;
+        
+        return esMio ;
+      });
+    }
+
+
+    if (this.segmentoActual === 'disponibles') {
+      return this.todasLasEntregas.filter(e => e.estatus === 1);
+    } else {
+      if (this.esAdmin) {
+        return this.todasLasEntregas.filter(e => e.estatus === 2);
+      }
+      return this.todasLasEntregas.filter(e => e.estatus === 2 && e.id_repartidor === this.idUsuarioActual);
+    }
   }
 
-  async deleteCategory(category: Entrega) {
-    await this.entregaDbRepository.deleteById(category.id_entrega);
+  async abrirSelectorVehiculo(entrega: any) {
+    const alert = await this.alertCtrl.create({
+      header: 'Seleccionar Vehículo',
+      inputs: [
+        { type: 'radio', label: 'Moto 01', value: 1, checked: true },
+        { type: 'radio', label: 'Camioneta', value: 2 }
+      ],
+      buttons: [
+        { text: 'Cancelar', role: 'cancel' },
+        { 
+          text: 'Confirmar Salida', 
+          handler: (idVehiculo) => this.asignarEntrega(entrega.id, idVehiculo) 
+        }
+      ]
+    });
+    await alert.present();
   }
 
+  asignarEntrega(idEntrega: number, idVehiculo: number) {
+    console.log('Asignando a repartidor:', this.idUsuarioActual, 'Vehiculo:', idVehiculo);
+    // Llamar a API -> UPDATE entregas SET id_repartidor = ?, id_vehiculo = ?, estatus = 2...
+  }
+
+  irAFormulario() {
+    this.router.navigate(['/entregas/form-entregas']);
+  }
+
+  marcarComoEntregado(datos: any) {
+    this.router.navigate(['/entregas/form-entregas']);
+  }
 }
--- src/app/services/preference.service.ts
+++ src/app/services/preference.service.ts
@@ -32,4 +32,30 @@ export class PreferencesService {
   async removeItem(key: string) {
     await Preferences.remove({ key: this.prefijo + '.' + key });
   }
+
+  async tienePermiso(modulo: string, accion: string): Promise<boolean> {
+    const data = await this.getItem('permisos');
+    if (!data) return false;
+    
+    const obj = JSON.parse(data);
+    return obj[modulo] && obj[modulo].includes(accion);
+  }
+
+  async esAdmin(): Promise<boolean> {
+    const data = await this.getItem('roles');
+    if (!data) return false;
+    
+    if (data.includes('ADMINISTRADOR')) 
+      return true;
+    else 
+      return false;
+  }
+
+  async getIdUser(): Promise<string> {
+    const data = await this.getItem('user');
+    if (!data) return '';
+
+    const obj = JSON.parse(data);
+    return obj.id;
+  }
 }
