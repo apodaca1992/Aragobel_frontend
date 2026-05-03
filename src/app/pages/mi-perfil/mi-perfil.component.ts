7626b80d450d2a83c7f0d864791748d291485046
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-05-03 11:34:18 -0700

    se agrego la seccion de mi perfil

--- src/app/pages/mi-perfil/mi-perfil.component.ts
+++ src/app/pages/mi-perfil/mi-perfil.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonList } from '@ionic/angular';
-import { ComponenteInterface } from '@interfaces/componente-interface';
-import { DeviceService } from '@services/device.service';
-import { GeolocationService } from '@services/geolocation.service';
+import { PreferencesService } from '@services/preference.service';
 
 @Component({
   selector: 'app-mi-perfil',
@@ -10,69 +8,25 @@ import { GeolocationService } from '@services/geolocation.service';
   styleUrls: ['./mi-perfil.component.scss'],
 })
 export class MiPerfilComponent  implements OnInit {
+  usuario: any = null;
 
   @ViewChild(IonList) ionList!: IonList;
 
-  componentes: ComponenteInterface[] = [
-    {
-      icon: 'heart',
-      name: 'prueba',
-      redirectTo: '/home'
-    },
-    {
-      icon: 'heart',
-      name: 'prueba1',
-      redirectTo: '/home'
-    }
-  ];
-  public position: string = 'Obteniendo ubicación...';
-
-  constructor(
-    private deviceService: DeviceService,
-    private geolocationService: GeolocationService
-    ) {}
+  constructor(private _preferencesService: PreferencesService) { }
 
   async ngOnInit() {
-    this.logDeviceInfo();
-    
-    await this.loadCurrentLocation();
-  }
-
-  private async logDeviceInfo() {
-    console.log(await this.deviceService.getDeviceId());
-    console.log(await this.deviceService.getModel());
-    console.log(await this.deviceService.getManufacturer());
-    console.log(await this.deviceService.getOperationSystem());
-    console.log(await this.deviceService.getOsVersion());
-    console.log(await this.deviceService.getPlatform());
-  }
-
-  private async loadCurrentLocation() {
-    const coords = await this.geolocationService.getPosition();
-
-    // AQUÍ ESTÁ EL TRUCO: Si coords no es nulo, TypeScript nos deja usarlo
-    if (coords) {
-      console.log('Ubicación recibida:', coords);
-      this.position = `Latitud: ${coords.latitude} | Longitud: ${coords.longitude}`;
-    } else {
-      // Si llegamos aquí, el servicio ya mostró un Toast con el error
-      this.position = 'Ubicación no disponible';
+    const userStr = await this._preferencesService.getItem('user');
+    if (userStr) {
+      this.usuario = JSON.parse(userStr);
+      console.log('Datos del perfil:', this.usuario);
     }
   }
 
-  favorite(user: any) {
-    console.log('favorite', user);
-    this.ionList.closeSlidingItems();
+  // Opcional: Función para formatear el nombre
+  get nombreCompleto() {
+    if (!this.usuario) return 'Cargando...';
+    return `${this.usuario.nombre} ${this.usuario.apellido_paterno} ${this.usuario.apellido_materno}`;
   }
 
-  share(user: any) {
-    console.log('share', user);
-    this.ionList.closeSlidingItems();
-  }
-
-  delete(user: any) {
-    console.log('delete', user.name);
-    this.ionList.closeSlidingItems();
-  }
 
 }
