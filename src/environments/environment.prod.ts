ab25aad2cfb428a1f04373ea32a44169c2353d1c
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-05-23 22:10:49 -0700

    se agrega url prod valida

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API: 'https://aragobel-backend-424137532274.us-central1.run.app', 
+  API: 'https://aragobel-backend-424137532274.us-central1.run.app/api', 
   prefijoLocalStorage: 'aragobel_prod',
   API_KEY:'',
   API_EMAIL:''
