cd86c3624484b2db4f26969ef5835937f83ce1a9
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-05-24 11:40:17 -0700

    se agrego un icono y splash a la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,16 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     androidScheme: 'https' // Este es el que debe coincidir con tu CORS
+  },
+  // 👈 Añade este bloque de configuración
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,         // Duración en milisegundos (2 segundos)
+      launchAutoHide: true,             // Se oculta automáticamente
+      backgroundColor: "#003399",       // Color de fondo subyacente (usa tu color de marca)
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false                // Opcional: true si quieres un círculo de carga nativo
+    }
   }
 };
 
