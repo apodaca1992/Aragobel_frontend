c8a3ce3dd303820ccff41c1f5ab678ab88ab9839
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-05-23 22:00:36 -0700

    se agregoo nueva url de la api desplegada en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API: 'https://aragobel-api-424137532274.us-central1.run.app/api', 
+  API: 'https://aragobel-backend-424137532274.us-central1.run.app', 
   prefijoLocalStorage: 'aragobel_prod',
   API_KEY:'',
   API_EMAIL:''
