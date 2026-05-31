21aebe70366f95cfa4f3b3dded4ba801101bcb0f
Jesus Adrian Apodaca Campos <jesus.apodaca@info.uas.edu.mx>
2026-05-31 15:40:11 -0700

    chore: configuracion de entornos y comandos multi-env

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API: 'https://aragobel-backend-qa-424137532274.us-central1.run.app/api', 
+  prefijoLocalStorage: 'aragobel_qa',
+  API_KEY:'',
+  API_EMAIL:''
+};
